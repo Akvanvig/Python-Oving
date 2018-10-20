814ba2b13e801fc8ccdca3a3052d87952db5dd83
akvanvig <akvanv@gmail.com>
2018-10-20 04:01:55 +0200

    Fullført oppg. 1 øving 6

--- IINI4020 Webprogrammering med Python/Oving 6/blogg/app.py
+++ IINI4020 Webprogrammering med Python/Oving 6/blogg/app.py
@@ -1,7 +1,7 @@
 """
-	Øving 6
-	16.10.2018
-	Anders Kvanvig
+        Øving 6
+        19.10.2018
+        Anders Kvanvig
 
     Python 3
 """
@@ -22,48 +22,99 @@ Bootstrap(app)
 app.config.from_object(__name__)
 
 #Åpner index-sida og viser nyheter/innlegg
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    db = get_db()
-    cur = db.execute('SELECT * FROM nyheter ORDER BY id DESC')
-    entries = cur.fetchall()
-    return render_template('index.html', entries=entries)
+        db = get_db()
+        cur = db.execute('SELECT n.tittel AS tittel, n.nyhet AS nyhet, b.navn AS forfatter FROM nyheter n JOIN brukere b ON n.forfatter_id = b.id ORDER BY n.id DESC')
+        entries = cur.fetchall()
+        cur = db.execute('SELECT * FROM brukere')
+        res = cur.fetchall()
+        return render_template('index.html',entries=entries,rader=res)
 
 
 #Åpner nyhetsskjema/Legger inn nyhet/innlegg i databasen
 @app.route('/opprett_artikkel', methods=['GET', 'POST'])
 def opprett_artikkel():
-	form = forms.ArtikkelSkjema(request.form)
-	if request.method == 'POST' and form.validate():
-		db = get_db()
-		db.execute(
-			'INSERT INTO nyheter (tittel, nyhet, forfatter) values (?, ?, ?)',
-			[request.form['tittel'], request.form['innhold'], request.form['forfatter']]
-		)
-		db.commit()
-		flash('innlegget ble sendt og lagret i databasen')
-		return redirect(url_for('index'))
-	return render_template('opprett_artikkel.html', form=form)
+        if 'brukernavn' not in session:
+                return redirect(url_for('login'))
+        form = forms.ArtikkelSkjema(request.form)
+        if request.method == 'POST' and form.validate():
+                db = get_db()
+                cur = db.execute('SELECT id FROM brukere WHERE navn = ? AND passord = ?', [session['brukernavn'], session['passord']])
+                rad = cur.fetchone()[0]
+                db.execute(
+                        'INSERT INTO nyheter (tittel, nyhet, forfatter_id) values (?, ?, ?)',
+                        [request.form['tittel'], request.form['innhold'], rad]
+                )
+                db.commit()
+                flash('innlegget ble sendt og lagret i databasen')
+                return redirect(url_for('index'))
+        return render_template('opprett_artikkel.html', form=form)
 
-#Legger inn en ny bruker i databasen
-@app.route('/add_bruker', methods=['POST'])
-def add_bruker():
-    db = get_db()
-    db.execute(
-        'INSERT INTO nyheter (tittel, nyhet, forfatter) values (?, ?, ?)',
-        [request.form['tittel'], request.form['nyhet'], request.form['forfatter']]
-    )
-    db.commit()
-    flash('innlegget ble sendt og lagret i databasen')
-    return redirect(url_for('index'))
 
 #Viser innloggingsskjema til bruker, og logger dem inn.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = forms.InnloggingSkjema(request.form)
-	if request.method == 'POST' and form.validate():
-		return 'Form Submitted'
-	return render_template('login.html', form=form)
+        form = forms.InnloggingSkjema(request.form)
+        if request.method == 'POST' and form.validate():
+                db = get_db()
+                brukernavn = request.form['brukernavn']
+                passord = request.form['passord']
+                cur = db.execute('SELECT COUNT(id) FROM brukere WHERE navn = ? AND passord = ?', [brukernavn, passord])
+                rad = cur.fetchone()[0]
+                if int(rad) > 0:
+                        session['brukernavn'] = brukernavn
+                        session['passord'] = passord
+                        session['innlogget'] = 1
+                        return redirect(url_for('index'))
+                flash('Feil brukernavn eller passord')
+        return render_template('login.html', form=form)
+
+
+#Legger inn en ny bruker i databasen
+@app.route('/opprett_bruker', methods=['GET', 'POST'])
+def opprett_bruker():
+        form = forms.BrukerSkjema(request.form)
+        if request.method == 'POST' and form.validate():
+                db = get_db()
+                brukernavn = request.form['brukernavn']
+                passord = request.form['passord']
+                cur = db.execute('SELECT COUNT(navn) FROM brukere WHERE navn = ?', [brukernavn])
+                rad = cur.fetchone()[0]
+                if int(rad) < 1:
+                    db.execute(
+                        'INSERT INTO brukere (navn, passord) values (?, ?)',
+                        [brukernavn, passord])
+                    db.commit()
+                    session['passord'] = passord
+                    session['brukernavn'] = brukernavn
+                    session['innlogget'] = 1
+                    flash('brukeren ble lagret i databasen')
+                    return redirect(url_for('index'))
+                flash('Brukeren eksisterer allerede')
+        return render_template('opprett_bruker.html', form=form)
+
+@app.route('/logg_ut')
+def logg_ut():
+    session.pop('brukernavn')
+    session.pop('passord')
+    session['innlogget'] = 0
+    return redirect(url_for('index'))
+
+@app.route('/header')
+def header():
+    return render_template('header.html')
+
+@app.route('/innlogging')
+def innlogging():
+        try:
+            if session['innlogget'] == None:
+                session['innlogget'] = 0
+        except Exception as e:
+            session['innlogget'] = 0
+        return render_template('innlogging.html', innlogget=session['innlogget'])
+
+
 
 #Database-----------------------------------------------------------------------------------------
 #Oppretter databasen
--- IINI4020 Webprogrammering med Python/Oving 6/blogg/forms.py
+++ IINI4020 Webprogrammering med Python/Oving 6/blogg/forms.py
@@ -1,17 +1,21 @@
 """
 	Øving 6
-	16.10.2018
+	19.10.2018
 	Anders Kvanvig
 
     Python 3
 """
-from wtforms import Form, StringField, PasswordField, validators
+from wtforms import Form, StringField, PasswordField, TextAreaField, validators
 
 class ArtikkelSkjema(Form):
-    tittel = StringField('tittel', [validators.DataRequired()])
-    innhold = StringField('innhold', [validators.DataRequired()])
-    forfatter = StringField('forfatter', [validators.DataRequired()])
+    tittel = StringField('tittel', [validators.Length(min=4, max=50)])
+    innhold = TextAreaField('innhold', [validators.Length(min=5)])
 
 class InnloggingSkjema(Form):
 	brukernavn = StringField('brukernavn', [validators.Length(min=4, max=25)])
-	passord = PasswordField('passord', [validators.Length(min=4, max=25)])
+	passord = PasswordField('passord', [validators.Length(min=4, max=35)])
+
+class BrukerSkjema(Form):
+	brukernavn = StringField('brukernavn', [validators.length(min=4, max=25)])
+	passord = PasswordField('passord', [validators.length(min=4, max=35), validators.EqualTo('gjenta', message='Passord må være like')])
+	gjenta = PasswordField('gjenta passord')
